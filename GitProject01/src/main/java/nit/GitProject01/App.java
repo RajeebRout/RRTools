cbd6f729ad114150f6b273adf4b39394072a3d95
routr <routr@192.168.1.52>
2025-08-21 19:04:46 +0530

    Developer Changed the Sum result

--- GitProject01/src/main/java/nit/GitProject01/App.java
+++ GitProject01/src/main/java/nit/GitProject01/App.java
@@ -9,7 +9,6 @@ public class App {
 	}
 	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         App ap =new App();
         System.out.println("Sum Result is : "+ap.sum(10, 20));
     }
