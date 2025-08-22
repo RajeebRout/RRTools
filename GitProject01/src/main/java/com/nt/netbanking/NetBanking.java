7e91ead07e6fb5e19ad58c05aaa88f014a2e51f3
routr <routr@192.168.1.52>
2025-08-22 19:03:40 +0530

    dev1 Account opening logic

--- GitProject01/src/main/java/com/nt/netbanking/NetBanking.java
+++ GitProject01/src/main/java/com/nt/netbanking/NetBanking.java
@@ -1,6 +1,11 @@
 package com.nt.netbanking;
 
+import java.util.Random;
+
 public class NetBanking 
 {
-
+	public String openAccount(String aadhar,double initialamount,String type)
+	{
+		return "Account is Opened with Acc no : "+new Random().nextLong(1321342123423L);
+	}
 }
