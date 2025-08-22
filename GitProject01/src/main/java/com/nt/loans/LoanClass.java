669cd43524549324aad2317bd194bea10ffdde41
routr <routr@192.168.1.52>
2025-08-22 21:17:44 +0530

    Developer created Loan package and class in Loan branch

---
+++ GitProject01/src/main/java/com/nt/loans/LoanClass.java
@@ -0,0 +1,11 @@
+package com.nt.loans;
+
+import java.util.Random;
+
+public class LoanClass 
+{		
+	public String loanAmount(String aadhar,double initialamount,String type)
+	{
+		return "Loan is sanctioned for the Account : "+new Random().nextLong(1321342123423L);
+	}
+}
