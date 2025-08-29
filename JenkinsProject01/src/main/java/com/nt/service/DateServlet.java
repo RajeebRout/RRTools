33cc2914507cc9e42bbf5cf5dd826061e013783b
routr <routr@192.168.1.50>
2025-08-29 12:24:20 +0530

    commit 2

--- JenkinsProject01/src/main/java/com/nt/service/DateServlet.java
+++ JenkinsProject01/src/main/java/com/nt/service/DateServlet.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/dateurl")
 public class DateServlet extends HttpServlet{
@@ -29,7 +29,7 @@ public class DateServlet extends HttpServlet{
 	}
 
 	@Override
-	protected void doPatch(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doGet(req,res);
 	}
 	
